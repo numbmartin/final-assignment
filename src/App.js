aa6118ca1ccdeae47df1eea022d774857bb4baa6
numbmartin <xxw603@miami.edu>
2022-05-06 02:18:05 -0400

    2222

--- src/App.js
+++ src/App.js
@@ -6,19 +6,22 @@ import Footer from './components/Footer';
 import Intro from './components/Intro';
 import Stories from './components/Stories';
 import Nav from './components/Nav';
+import { HashRouter, Routes, Route} from "react-router-dom"
 
 
 function App() {
   return (
-    <>
+    <HashRouter>
     <Nav/>
-    <Intro/>
-    <Stories/>
-    <About/>
-    <Contact/>
+    <Routes>
+      <Route path= {"/"} element= {<Intro/>}/>
+      <Route path= {"/portfolio"} element= {<Stories/>}/>
+      <Route path= {"/about"} element= {<About/>}/>
+      <Route path= {"/contact"} element= {<Contact/>}/>
+    </Routes>
     <Footer/>
 
-    </>
+    </HashRouter>
   );
 }
 
