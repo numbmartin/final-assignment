39e2874b980d48763c95995d22a7aca1d07571eb
numbmartin <xxw603@miami.edu>
2022-05-06 01:55:07 -0400

    222

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -7,7 +7,7 @@ function Nav() {
     return(
         <>
         <nav className="navbar navbar-light navbar-expand-lg fixed-top bg-secondary text-uppercase" id="mainNav">
-        <div className="container"><Link className="navbar-brand" to="#page-top">Brand</Link><button dataBsToggle="collapse" dataBsTarget="#navbarResponsive" className="navbar-toggler text-white bg-primary navbar-toggler-right text-uppercase rounded" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i className="fa fa-bars"></i></button>
+        <div className="container"><Link className="navbar-brand" to="/page-top">Brand</Link><button dataBsToggle="collapse" dataBsTarget="#navbarResponsive" className="navbar-toggler text-white bg-primary navbar-toggler-right text-uppercase rounded" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i className="fa fa-bars"></i></button>
             <div className="collapse navbar-collapse" id="navbarResponsive">
                 <ul className="navbar-nav ms-auto">
                     <li className="nav-item mx-0 mx-lg-1"><Link className="nav-link py-3 px-0 px-lg-3 rounded" to="/portfolio">Portfolio</Link></li>
